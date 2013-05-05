144dad128a2651abbf9ad7feb099d17d264aa6dc
James Hwang <james.hwang.usc@gmail.com>
2013-05-05 00:19:50 -0700

    Add all tiers and prices

--- lib/product.rb
+++ lib/product.rb
@@ -7,6 +7,14 @@ class Product
     case @type
     when 'free'
       0
+    when 'bronze'
+      10
+    when 'silver'
+      25
+    when 'gold'
+      50
+    when 'platinum'
+      100
     end
   end
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,16 +1,23 @@
 require 'spec_helper'
 
 describe Product do
-  let(:free) { Product.new('free') }
-  let(:bronze) { Product.new('bronze')}
-  let(:silver) { Product.new('silver')}
-  let(:gold) { Product.new('gold')}
-  let(:platinum) { Product.new('platinum')}
+  @free     = Product.new('free')
+  @bronze   = Product.new('bronze')
+  @silver   = Product.new('silver')
+  @gold     = Product.new('gold')
+  @platinum = Product.new('platinum')
 
-  context "for free products" do
-    it "price is 0" do
-      free.price.should eq 0
+  [
+    {:product => @free, :name => 'free', :price => 0},
+    {:product => @bronze, :name => 'bronze', :price => 10},
+    {:product => @silver, :name => 'silver', :price => 25},
+    {:product => @gold, :name => 'gold', :price => 50},
+    {:product => @platinum, :name => 'platinum', :price => 100}
+  ].each do |tiers|
+    context "for #{tiers[:name]} products" do
+      it "price is #{tiers[:price]}" do
+        tiers[:product].price.should eq tiers[:price]
+      end
     end
   end
-
 end
